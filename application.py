257e8006cf80b46fc85c9ffcb647b5693b1f9c38
nithish687 <124697697+nithish687@users.noreply.github.com>
2023-04-14 03:58:29 +0000

    middle

--- application.py
+++ application.py
@@ -1,10 +1,22 @@
 from flask import Flask,render_template
-
+from database import engine
+from sqlalchemy import text
 application  = Flask(__name__)
 
+def user_names_db():
+ with engine.connect() as conn:
+  result = conn.execute(text("select * from details"))
+  user_names = []
+  for name in result.all():
+    user_names.append(dict(name))
+  return user_names
+
 @application.route("/")
+
 def app():
-  return render_template('homepage.html')
+  user_name = user_names_db()
+  return render_template('homepage.html',user_name = user_name)
+  
 if __name__ == "__main__":
   application.run(host='0.0.0.0',debug=True)
 
--- database.py
+++ database.py
@@ -1,11 +1,14 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://2udg12wufjre0c4ycdlh:pscale_pw_JAtZn9AjDIknTMO6fASUG0aRdiAmk3bQvBRxRy8WqvD@aws.connect.psdb.cloud/nitish?charset=utf8mb4",connect_args={
+engine = create_engine("mysql+pymysql://4tgzmpc9q977lyyedh0m:pscale_pw_tcXTpnJVA4Rp6aE5T7JnMH1PzlZoz3mN9boy2fhvc3W@aws.connect.psdb.cloud/nitish?charset=utf8mb4",
+connect_args={
   "ssl": {
     "ssl_ca": "/etc/ssl/cert.pem"
   }
 })
 
+
 with engine.connect() as conn:
   result = conn.execute(text("select * from details"))
-  print(result.all())
+  return result.all()
+
